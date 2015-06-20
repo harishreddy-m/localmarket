df1fff66968db7dcac2c164ba371e2313ac10d62
harish-myaccount <hreddy@3clogic.com>
2015-06-20 12:07:36 +0530

    Basic

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/hello/:name', function(req, res){
   res.send('hello ' + req.params.name);
 });
 
+app.get('/', function(req, res){
+  res.send('Now time is  ' + new Date());
+});
+
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3030;
 app.listen(port,ip,function(){
