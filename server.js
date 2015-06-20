ca95e9d9d9ca3056591ce591ea06eb34d308f1ec
harish-myaccount <hreddy@3clogic.com>
2015-06-20 11:21:11 +0530

    Ip added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/hello/:name', function(req, res){
   res.send('hello ' + req.params.name);
 });
 
-var ip = process.env.OPENSHIFT_NODEJS_PORT || 3030
-app.listen(ip);
+var ip = process.env.OPENSHIFT_NODEJS_IP || 127.0.0.1
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3030
+app.listen(port,ip);
 console.log('Listening on port ' + ip);
