d1f4fb77fc68511dbc3d0c97f2da2ad21ca73d6b
harish-myaccount <hreddy@3clogic.com>
2015-06-20 11:16:16 +0530

    Port

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ app.get('/hello/:name', function(req, res){
   res.send('hello ' + req.params.name);
 });
 
-app.listen(process.env.OPENSHIFT_INTERNAL_PORT);
-console.log('Listening on port ' + process.env.OPENSHIFT_INTERNAL_PORT);
+var ip = process.env.OPENSHIFT_NODEJS_PORT || 3030
+app.listen(ip);
+console.log('Listening on port ' + ip);
