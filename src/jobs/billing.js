fc591683276e73bcaa24fd5afb430dd28df28fd6
harish-myaccount <hreddy@3clogic.com>
2015-07-30 12:13:48 +0530

    minor change

--- src/jobs/billing.js
+++ src/jobs/billing.js
@@ -1,10 +1,10 @@
 
 var Agenda = require('agenda');
 var config = require('config');
- var dbConfig; 
+var dbConfig; 
 
- var env = process.env.NODE_ENV || 'development';
- if ('development' == env) {
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
     dbConfig = config.get('App.dbConfig.url');
 }else{
     dbConfig = "mongodb://"+config.get('App.dbConfig.user')+":"+config.get('App.dbConfig.password')+"@"+process.env.OPENSHIFT_MONGODB_DB_HOST+":"+process.env.OPENSHIFT_MONGODB_DB_PORT+"/localmarket";
@@ -14,36 +14,35 @@ var Customer = require('../models/customer');
 var Item = require('../models/item');
 var BilledOrder = require('../models/billedorder');
 agenda.define('generate bills for orders', function(job, done) {
-console.log('Generating bill for ' + new Date());
+    console.log('Generating bill for ' + new Date());
 
-Customer.find({}).select("orders").populate("orders").exec(function(error,customers){
+    Customer.find({}).select("orders").populate("orders").exec(function(error,customers){
         if(error)
             console.log(error);
         else{
             Item.populate(customers,{path: 'orders.item' },function(){
                 for(var i=0;i<customers.length;i++){
-                var billorder = new BilledOrder({billingdate : new Date(),customer:customers[i]._id,billamount:0});
-
-                 var billamount=0;
-                 for(var j=0;j<customers[i].orders.length;j++){                    
+                    var dailyorder = new BilledOrder({billingdate : new Date(),customer:customers[i]._id,billamount:0})
+                   for(var j=0;j<customers[i].orders.length;j++){                    
                     if(customers[i].orders[j].frequency=='daily'){
-                        billorder.orders.push(customers[i].orders[j]._id);
-                        billamount=billamount+(customers[i].orders[j].quantity*customers[i].orders[j].item.price);
+                        dailyorder.orders.push(customers[i].orders[j]._id);
+                        dailyamount=dailyamount+(customers[i].orders[j].quantity*customers[i].orders[j].item.price);
                     }else if(customers[i].orders[j].frequency=='once'){
-
+                        var billorder = new BilledOrder({billingdate : new Date(),customer:customers[i]._id,billamount:(customers[i].orders[j].quantity*customers[i].orders[j].item.price)});
+                        billorder.orders.push(customers[i].orders[j]._id);
+                        billorder.save(function(error){
+                            if(error)
+                                console.log(error);
+                        });
                     }else if(customers[i].orders[j].frequency=='monthly'){
 
                     }
-                 }
-                 billorder.billamount = billamount;
-                 billorder.save(function(error){
-                    if(error)
-                    console.log(error);
-                 });
+                    dailyorder.billamount=dailyamount;
+                }
             }
-            })
-        }
-    });
+        })
+}
+});
 done();
 });
 
